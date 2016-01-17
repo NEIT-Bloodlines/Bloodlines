b8e6dafb763204c854d782f437859803ba33348e
David Landi <djlandi@email.neit.edu>
2016-01-17 12:05:47 -0500

    landing page and default nav initial commit

--- Bloodlines/View/landingContent.php
+++ Bloodlines/View/landingContent.php
@@ -1,8 +1,62 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Developer: David Landi 1/16/2016
+ * Purpose: 
+ *  Contains all of the html elements for the landing page. The controller for
+ *  the landing page is the root index.php file.
+ * Requirements:
+ *  To build the signin page the php controller will need to add the 
+ *  header, dfltNav, landingContent and then footer.
+ * TODO: 
+ *  1. php get data for player rankings section
+ *  2. php routing functionality 
+ *  3. css class will need to be added for each class that contains underscore
  */
+?>
+<div class="container-fluid main_container">
+    
+<div class="row">
+<div class="col-md-12">
+    <section id ="landing">
+        <div class="container landing_content">
+
+        </div>
+    </section>
+</div>
+</div>
+    
+<div class="row">
+<div class="col-md-12">
+    <section id ="about">
+        <div class="container landing_content">
+            <p class="bl_logo">BLOODLINES</p>
+            <p class="bl_slogan">Will your family's crest be remembered?</p>
+            <button type="button" class="btn btn-success btn_cust" id="started" title="prop section">
+                GET STARTED
+            </button>
+        </div>
+    </section>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-12">
+    <section id ="manual">
+        <div class="container landing_content">
+
+        </div>
+    </section>
+</div>
+</div>
+    
+<div class="row">
+<div class="col-md-12">
+    <section id ="rankings">
+        <div class="container landing_content">
+
+        </div>
+    </section>
+</div>
+</div>
+    
+</div>
 
