fe3b61a50d767b56758a0010ac41a5e7fe26ff19
David Landi <djlandi@email.neit.edu>
2016-02-06 13:10:21 -0500

    Added javascript event handlers for authentication page

---
+++ Bloodlines/views/PreGame/preGameNavBar.php
@@ -0,0 +1,50 @@
+<?php
+/* 
+ * Developer: David Landi
+ * Created: 2/6/16
+ * Purpose: Navagation Bar for the preGame page
+ * TODO: 
+ *      - Add links 
+ * Modifed by: (Name, Reason, Date)
+ *  */
+?>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+<div class="container">
+<div class="navbar-header page-scroll">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nd_collapse">
+<span class="sr-only">Toggle navigation</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="#page-top">Bloodlines</a>
+</div>
+<div class="collapse navbar-collapse" id="nd_collapse">
+<ul class="nav navbar-nav navbar-right">
+<li class="hidden">
+<a href="#page-top"></a>
+</li>
+<li class="page-scroll">
+<a href="#">Main Lobby</a>
+</li>
+<li class="page-scroll">
+<a href="#">Host Game</a>
+</li>
+<li class="page-scroll">
+<a href="#">Join Game</a>
+</li>
+<li>
+<a href="#">Manual</a>
+</li>
+<li>
+<a href="#">Help</a>
+</li>
+<li>
+<a href="#">Sign Out</a>
+</li>
+</ul>
+</div>
+</div>
+</nav>
+
