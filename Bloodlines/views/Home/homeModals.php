4062f80343c978e292472150f6635525c7465dc1
David Landi <david.landi@vectorcast.com>
2016-02-18 10:55:30 -0500

    added home modals

--- Bloodlines/views/Home/homeModals.php
+++ Bloodlines/views/Home/homeModals.php
@@ -8,147 +8,281 @@
  * Modifed by: (Name, Reason, Date)
  *  */
 ?>
-<!-- PHP & mySQL MODAL -->
-<div id="member_login_help_popup" class="modal fade dflt_modal_style">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content text-justify">
-            <div class="modal-header text-center">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
-                </button>
-                <h4><strong>PHP & mySQL</strong></h4>
-            </div><!--/modal-header-->
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- /PHP & mySQL MODAL -->
 
-<!-- Jquery & Ajax MODAL -->
-<div id="member_login_help_popup" class="modal fade dflt_modal_style">
+<div id="ss1FullView" class="modal fade dflt_modal_style">
     <div class="modal-dialog modal-lg">
         <div class="modal-content text-justify">
             <div class="modal-header text-center">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
                 </button>
-                <h4><strong>Jquery & Ajax</strong></h4>
+                <h4><strong>TODO: add img title here</strong></h4>
             </div><!--/modal-header-->
+            
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
-                    </div>
-                </div>
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
             </div>
         </div>
     </div>
 </div>
-<!-- /Jquery & Ajax MODAL -->
 
-<!-- Frontend Design -->
-<div id="member_login_help_popup" class="modal fade dflt_modal_style">
+<div id="ss2FullView" class="modal fade dflt_modal_style">
     <div class="modal-dialog modal-lg">
         <div class="modal-content text-justify">
             <div class="modal-header text-center">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
                 </button>
-                <h4><strong>Frontend Design</strong></h4>
+                <h4><strong>TODO: add img title here</strong></h4>
             </div><!--/modal-header-->
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
-                    </div>
-                </div>
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
             </div>
         </div>
     </div>
 </div>
-<!-- /Frontend Design -->
 
-<!-- Custom MVC Framework -->
-<div id="member_login_help_popup" class="modal fade dflt_modal_style">
+<div id="ss3FullView" class="modal fade dflt_modal_style">
     <div class="modal-dialog modal-lg">
         <div class="modal-content text-justify">
             <div class="modal-header text-center">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
                 </button>
-                <h4><strong>Custom MVC Framework</strong></h4>
+                <h4><strong>TODO: add img title here</strong></h4>
             </div><!--/modal-header-->
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <p><strong>Not a member?</strong></p>
-                        <p>TODO: Enter text for info on how to become a member</p>
-                        <hr/>
-                        <p><strong>Cant remember username or password?</strong></p>
-                        <p>TODO: Enter info in response to questions</p>
-                    </div>
-                </div>
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
             </div>
         </div>
     </div>
 </div>
-<!-- /Custom MVC Framework -->
 
-<!-- Project Management -->
-<div id="member_login_help_popup" class="modal fade dflt_modal_style">
+<div id="ss4FullView" class="modal fade dflt_modal_style">
     <div class="modal-dialog modal-lg">
         <div class="modal-content text-justify">
             <div class="modal-header text-center">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
                 </button>
-                <h4><strong>Project Management</strong></h4>
+                <h4><strong>TODO: add img title here</strong></h4>
             </div><!--/modal-header-->
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
-                    </div>
-                </div>
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
             </div>
         </div>
     </div>
 </div>
-<!-- /Project Management -->
 
-<!-- HTML 5 Canvas -->
-<div id="member_login_help_popup" class="modal fade dflt_modal_style">
+<div id="ss5FullView" class="modal fade dflt_modal_style">
     <div class="modal-dialog modal-lg">
         <div class="modal-content text-justify">
             <div class="modal-header text-center">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
                 </button>
-                <h4><strong>HTML 5 Canvas</strong></h4>
+                <h4><strong>TODO: add img title here</strong></h4>
             </div><!--/modal-header-->
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
-                    </div>
-                </div>
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
             </div>
         </div>
     </div>
 </div>
-<!-- /HTML 5 Canvas -->
 
-<!-- Github -->
-<div id="member_login_help_popup" class="modal fade dflt_modal_style">
+<div id="ss6FullView" class="modal fade dflt_modal_style">
     <div class="modal-dialog modal-lg">
         <div class="modal-content text-justify">
             <div class="modal-header text-center">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
                 </button>
-                <h4><strong>Github</strong></h4>
+                <h4><strong>TODO: add img title here</strong></h4>
             </div><!--/modal-header-->
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
+                <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="dlSummary" class="modal fade dflt_modal_style">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-justify">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
+                </button>
+                <h3><strong>David Landi | Summary of Work</strong></h3>
+            </div><!--/modal-header-->
+            <div class="modal-body">
+                <p>TODO: Add content</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="npSummary" class="modal fade dflt_modal_style">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-justify">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
+                </button>
+                <h3><strong>Nick Patel | Summary of Work</strong></h3>
+            </div><!--/modal-header-->
+            <div class="modal-body">
+                <p>TODO: Add content</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="jbSummary" class="modal fade dflt_modal_style">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-justify">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
+                </button>
+                <h3><strong>Justin Beraera | Summary of Work</strong></h3>
+            </div><!--/modal-header-->
+            <div class="modal-body">
+                <p>TODO: Add content</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="dlCode" class="modal fade dflt_modal_style">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-justify">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
+                </button>
+                <h3><strong>Nick Patel</strong></h3>
+            </div><!--/modal-header-->
+            <div class="modal-body">
+                <p>TODO: Add content</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="npCode" class="modal fade dflt_modal_style">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-justify">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
+                </button>
+                <h3><strong>Nick Patel</strong></h3>
+            </div><!--/modal-header-->
+            <div class="modal-body">
+                <p>TODO: Add content</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="jbCode" class="modal fade dflt_modal_style">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-justify">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
+                </button>
+                <h3><strong>Nick Patel</strong></h3>
+            </div><!--/modal-header-->
+            <div class="modal-body">
+                <p>TODO: Add content</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="dlEmail" class="modal fade dflt_modal_style">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-justify">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
+                </button>
+                <h3><strong>David Landi | dlandi@bloodlines.tk</strong></h3>
+            </div><!--/modal-header-->
+            <div class="modal-body">
+                <div class="modal-body">
+                    <div class="input-group frm_input_space">
+                        <span class="input-group-addon" id="basic-addon1"><i class="fa fa-user"></i></span>
+                        <input type="text" class="form-control" id="txtUsrNameDL" placeholder="Name" aria-describedby="basic-addon1">
                     </div>
+                    <div class="input-group frm_input_space">
+                        <span class="input-group-addon" id="basic-addon2"><i class="fa fa-user"></i></span>
+                        <input type="text" class="form-control" id="txtUsrEmailDl" placeholder="Email" aria-describedby="basic-addon2">
+                    </div>
+                    <div class="input-group frm_input_space">
+                        <span class="input-group-addon" id="basic-addon3"><i class="fa fa-user"></i></span>
+                        <input type="text" class="form-control" id="txtSubjectDL" placeholder="Subject" aria-describedby="basic-addon3">
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Message:</label>
+                        <textarea class="form-control" rows="5" id="txtUsrMessageDl"></textarea>
+                    </div>
+                    <button type="button" class="btn btn-block btn-primary frm_btn" id="btnSendDLEmail">Send</button> 
                 </div>
             </div>
         </div>
     </div>
 </div>
-<!-- /Github -->
+
+<div id="jbEmail" class="modal fade dflt_modal_style">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-justify">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
+                </button>
+                <h3><strong>Justin Bareara | jbareara@bloodlines.tk</strong></h3>
+            </div><!--/modal-header-->
+            <div class="modal-body">
+                <div class="modal-body">
+                    <div class="input-group frm_input_space">
+                        <span class="input-group-addon" id="basic-addon1"><i class="fa fa-user"></i></span>
+                        <input type="text" class="form-control" id="txtUsrNameJB" placeholder="Name" aria-describedby="basic-addon1">
+                    </div>
+                    <div class="input-group frm_input_space">
+                        <span class="input-group-addon" id="basic-addon2"><i class="fa fa-user"></i></span>
+                        <input type="text" class="form-control" id="txtUsrEmailJB" placeholder="Email" aria-describedby="basic-addon2">
+                    </div>
+                    <div class="input-group frm_input_space">
+                        <span class="input-group-addon" id="basic-addon3"><i class="fa fa-user"></i></span>
+                        <input type="text" class="form-control" id="txtSubjectJB" placeholder="Subject" aria-describedby="basic-addon3">
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Message:</label>
+                        <textarea class="form-control" rows="5" id="txtUsrMessageJB"></textarea>
+                    </div>
+                    <button type="button" class="btn btn-block btn-primary frm_btn" id="btnSendJBEmail">Send</button> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="npEmail" class="modal fade dflt_modal_style">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-justify">
+            <div class="modal-header text-center">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;
+                </button>
+                <h3><strong>Nick Patel | npatel@bloodlines.tk</strong></h3>
+            </div><!--/modal-header-->
+            <div class="modal-body">
+                <div class="input-group frm_input_space">
+                    <span class="input-group-addon" id="basic-addon1"><i class="fa fa-user"></i></span>
+                    <input type="text" class="form-control" id="txtUsrNameNP" placeholder="Name" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group frm_input_space">
+                    <span class="input-group-addon" id="basic-addon2"><i class="fa fa-user"></i></span>
+                    <input type="text" class="form-control" id="txtUsrEmailNP" placeholder="Email" aria-describedby="basic-addon2">
+                </div>
+                <div class="input-group frm_input_space">
+                    <span class="input-group-addon" id="basic-addon3"><i class="fa fa-user"></i></span>
+                    <input type="text" class="form-control" id="txtSubjectNP" placeholder="Subject" aria-describedby="basic-addon3">
+                </div>
+                <div class="form-group">
+                    <label for="comment">Message:</label>
+                    <textarea class="form-control" rows="5" id="txtUsrMessageNP"></textarea>
+                </div>
+                <button type="button" class="btn btn-block btn-primary frm_btn" id="btnSendNPEmail">Send</button> 
+            </div>
+        </div>
+    </div>
+</div>
