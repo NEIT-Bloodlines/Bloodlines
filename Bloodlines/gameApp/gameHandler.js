e207b042791ac00cb5653c72949f36d0846556fe
David Landi <david.landi@vectorcast.com>
2016-02-23 16:50:34 -0500

    initial commit for gameApp

---
+++ Bloodlines/gameApp/gameHandler.js
@@ -0,0 +1,8 @@
+/*
+ * Developer: David Landi
+ * Created: 2/23/16
+ * Purpose: JS ajax calls to php script
+ * Modifed by: (Name, Reason, Date)
+ *  */
+
+
---
+++ Bloodlines/gameApp/userData.js
@@ -0,0 +1,22 @@
+/*
+ * Developer: David Landi
+ * Created: 2/23/16
+ * Purpose: JS methods for storing gamedata to localstorage
+ * Modifed by: (Name, Reason, Date)
+ *  */
+var GameSettingsData = (function(){
+    var gameSettings = {};
+    return{};
+})();
+
+var TerritoryData = (function(){
+    return{};
+})();
+
+var PlayerData = (function(){
+    return{};
+})();
+
+var DataUtil = (function(){
+    return{};
+})();
